07a6e33c5fa1bfe887c5bfcc3bc119a7d41b0201
Niall <niall.bambury@codurance.com>
2021-10-10 12:07:01 +0100

    [NB] Update user table to use auto increment primary key and use it by default

--- user.sql
+++ user.sql
@@ -1,10 +1,9 @@
 CREATE TABLE public.example_user (
-    id SERIAL NOT NULL, 
+    id SERIAL PRIMARY KEY, 
     name varchar(100) NOT NULL, 
     age int NOT NULL,
     dateofbirth DATE NOT NULL
 );
 
-INSERT INTO public.example_user(id, name, age, dateofbirth)
-VALUES (1,'John Doe',26,'1970-12-31')
-RETURNING *;
+INSERT INTO public.example_user(name, age, dateofbirth)
+VALUES ('John Doe',26,'1970-12-31');
