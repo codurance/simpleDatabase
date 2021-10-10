5b2e1ffca81b0131bed16f915f71b485dd0e56cd
Niall <niall.bambury@codurance.com>
2021-10-10 13:43:20 +0100

    [NB] Add exercise details to user sql file

--- user.sql
+++ user.sql
@@ -1,3 +1,5 @@
+-- no peeking! ಠ_ಠ
+
 CREATE TABLE public.example_user (
     id SERIAL PRIMARY KEY, 
     name varchar(100) NOT NULL, 
@@ -14,3 +16,14 @@ CREATE TABLE public.product (
 INSERT INTO public.example_user(name, age, dateofbirth) VALUES ('John Doe',26,'1970-12-31');
 INSERT INTO public.example_user(name, age, dateofbirth) VALUES ('Jane Smith',45,'1966-12-31');
 INSERT INTO public.product (userId,description) VALUES (2,'Plasma screen tv');
+
+-- Exercise 1
+-- DELETE FROM example_user WHERE id = 2
+
+-- Exercise 2
+/*
+    BEGIN;
+    INSERT INTO product (userId, description) VALUES (1, 'clock');
+    ROLLBACK;
+    COMMIT;
+*/
