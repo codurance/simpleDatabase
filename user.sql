08b341cb16fae1e84026ba5a3c2ee7c50aba4919
Niall <niall.bambury@codurance.com>
2021-10-10 12:43:42 +0100

    [NB] Add product table with foreign key example

--- user.sql
+++ user.sql
@@ -5,5 +5,12 @@ CREATE TABLE public.example_user (
     dateofbirth DATE NOT NULL
 );
 
-INSERT INTO public.example_user(name, age, dateofbirth)
-VALUES ('John Doe',26,'1970-12-31');
+CREATE TABLE public.product (
+    id SERIAL PRIMARY KEY,
+    userId int REFERENCES example_user (id),
+    description varchar(100) NOT NULL
+);
+
+INSERT INTO public.example_user(name, age, dateofbirth) VALUES ('John Doe',26,'1970-12-31');
+INSERT INTO public.example_user(name, age, dateofbirth) VALUES ('Jane Smith',45,'1966-12-31');
+INSERT INTO public.product (userId,description) VALUES (2,'Plasma screen tv');
